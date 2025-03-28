310f5fd06784e9578768e411ee4d3c4d449d4563
mahmoudadel810 <mahmoudadel810@gmail.com>
2025-03-28 15:39:20 +0200

    Adding Login Feature

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -10,6 +10,115 @@ import NotFound from "./notFound";
 import About from "./about";
 import Contact from "./contact";
 import Menu from "./menu";
+import Login from "./login";
+
+
+// class App extends Component {
+//   state = {
+//     products: [
+//       { id: 1, name: "Burger", count: 0, price: 30, isInCart: false },
+//       { id: 2, name: "Fries", count: 0, price: 20, isInCart: false },
+//       { id: 3, name: "Cola", count: 0, price: 10, isInCart: false }
+//     ]
+//   };
+
+//   handleReset = () => {
+//     //Clone
+//     let products = [...this.state.products];
+//     //Edit
+//     products = products.map((p) => {
+//       p.count = 0;
+//       return p;
+//     });
+//     //Set state
+//     this.setState({ products });
+//   };
+
+//   IncrementHandler = (product) => {
+//     //Clone
+//     const products = [...this.state.products];
+//     const index = products.indexOf(product);
+//     products[index] = { ...products[index] };
+//     //Edit
+//     products[index].count++;
+//     //Set State
+//     this.setState({ products });
+//   };
+
+//   handleDelete = (product) => {
+//     //Clone
+//     const products = [...this.state.products];
+//     const index = products.indexOf(product);
+//     //Edit
+//     products.splice(index, 1);
+//     //Set State
+//     this.setState({ products });
+//   };
+
+//   handleInCartChange = (product) => {
+//     //Clone
+//     const products = [...this.state.products];
+//     const index = products.indexOf(product);
+//     products[index] = { ...products[index] };
+//     //Edit
+//     products[index].isInCart = !products[index].isInCart;
+//     //Set State
+//     this.setState({ products });
+//   };
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <NavBar
+//           productsCount={this.state.products.filter((p) => p.isInCart).length}
+//         />
+//         <main className="container">
+//           <Switch>
+//             <Route
+//               path="/products/:id/:name?"
+//               render={(props) => (
+//                 <ProductDetails products={this.state.products} {...props} />
+//               )}
+//             />
+//             <Route
+//               path="/cart"
+//               render={(props) => (
+//                 <ShoppingCart
+//                   products={this.state.products.filter((p) => p.isInCart)}
+//                   onIncrement={this.IncrementHandler}
+//                   onDelete={this.handleInCartChange}
+//                   onReset={this.handleReset}
+//                   {...props}
+//                 />
+//               )}
+//             />
+//             <Route path="/notfound" component={NotFound} />
+//             {/* //2. Add Route */}
+//             <Route
+//               path="/menu"
+//               render={(props) => (
+//                 <Menu
+//                   {...props}
+//                   products={this.state.products}
+//                   onClick={this.handleInCartChange}
+//                 />
+//               )}
+//             />
+//             <Route path="/home" exact component={Home} />
+//             <Redirect from="/" to="/home" />
+//             <Redirect to="/notfound" />
+//           </Switch>
+//           {/* <ShoppingCart
+//             products={this.state.products}
+//             onIncrement={this.IncrementHandler}
+//             onDelete={this.handleDelete}
+//             onReset={this.handleReset}
+//           /> */}
+//         </main>
+//       </React.Fragment>
+//     );
+//   }
+// }
 
 class App extends Component {
   state = {
@@ -78,6 +187,7 @@ class App extends Component {
                 <ProductDetails products={this.state.products} {...props} />
               )}
             />
+            <Route path="/login" component={Login} />
             <Route
               path="/cart"
               render={(props) => (
@@ -91,7 +201,6 @@ class App extends Component {
               )}
             />
             <Route path="/notfound" component={NotFound} />
-            {/* //2. Add Route */}
             <Route
               path="/menu"
               render={(props) => (
