2e0598c3af3475380395632205949b668a755742
mahmoudadel810 <mahmoudadel810@gmail.com>
2025-03-29 17:42:47 +0200

    Adding Back End feture axios to get data from

--- src/components/shoppingCart.jsx
+++ src/components/shoppingCart.jsx
@@ -5,7 +5,23 @@ import React, { Component } from "react";
 import Product from "./product";
 
 class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Shopping Cart ==> Constructor");
+  }
+
+  componentDidMount() {
+    //Call Backend Server
+    console.log("Shopping Cart ==> ComponentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Shopping Cart ==> ComponentDidUpdate");
+    console.log(prevProps);
+  }
+
   render() {
+    console.log("Shopping Cart ==> Render");
     return (
       <React.Fragment>
         <h1>Shopping Cart</h1>
